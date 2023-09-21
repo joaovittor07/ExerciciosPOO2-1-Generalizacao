773d7850a8fa988c6c6893d89359084ef1c8e193
joaovittor07 <119264762+joaovittor07@users.noreply.github.com>
2023-09-21 15:50:05 -0300

    Add files via upload

---
+++ Atividade2/Veiculo/Caminhao.java
@@ -0,0 +1,29 @@
+package Veiculo;
+
+public class Caminhao extends Veiculo{
+	
+	private int eixos;
+
+	public Caminhao(boolean motor, int eixos) {
+		super(motor);
+		this.eixos = eixos;
+	}
+
+	public int getEixos() {
+		return eixos;
+	}
+
+	public void setEixos(int eixos) {
+		this.eixos = eixos;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Caminhao [eixos=");
+		builder.append(eixos);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+}
